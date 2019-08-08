0f390d3f813afe47ae1a9d2cc5da4eb09b33689f
Stefan Siegl <stesie@brokenpipe.de>
2019-08-09 00:04:37 +0200

    provide user fixture

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class UserFixture extends Fixture
+{
+    /**
+     * @var UserPasswordEncoderInterface
+     */
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('editor@example.org');
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'editor_password'));
+
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
