284055c96c7c46c49ed1e5b8efb4ebb8ee86a476
Stefan Siegl <stesie@brokenpipe.de>
2020-01-12 20:44:24 +0100

    set manifest key prefix

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ Encore
     // public path used by the web server to access the output path
     .setPublicPath((process.env.APP_PUBLIC_PREFIX || '') + '/build')
     // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .setManifestKeyPrefix('build/')
 
     /*
      * ENTRY CONFIG
