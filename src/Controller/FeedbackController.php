99eb8a4b19d020d274f1c7454b51152345dcc851
Stefan Siegl <stesie@brokenpipe.de>
2020-01-24 12:56:46 +0100

    add pdf generator, closes #11

--- src/Controller/FeedbackController.php
+++ src/Controller/FeedbackController.php
@@ -6,6 +6,7 @@ use App\Entity\Feedback;
 use App\Entity\Session;
 use App\Entity\User;
 use App\Form\FeedbackType;
+use App\Service\FeedbackService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -55,5 +56,18 @@ class FeedbackController extends AbstractController
     public function thanks()
     {
         return $this->render('feedback/thanks.html.twig');
+    }
+
+    /**
+     * @Route("/session/{id}/pdf", name="feedback_pdf", methods={"GET"})
+     * @param Session $session
+     * @param FeedbackService $feedbackService
+     * @return Response
+     */
+    public function pdf(Session $session, FeedbackService $feedbackService): Response
+    {
+        return Response::create($feedbackService->generatePdf($session), Response::HTTP_OK, [
+            'Content-type' => 'application/pdf',
+        ]);
     }
 }
