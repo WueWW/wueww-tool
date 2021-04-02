6eb8acd984bec483ba52c83cb25413a92105fe87
Stefan Siegl <stesie@brokenpipe.de>
2021-04-02 12:26:50 +0200

    adapt import due to deprecation

--- src/Repository/FeedbackRepository.php
+++ src/Repository/FeedbackRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Feedback;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Feedback|null find($id, $lockMode = null, $lockVersion = null)
